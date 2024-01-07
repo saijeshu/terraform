cf6e9c05f7050a1157a1dfc251cf1048099c6920
github.com <dhanukondasandhya8@gmail.com>
2024-01-07 16:24:46 +0530

    adding vaiables

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "ami_id" {
+  type = string
+  default = "ami-03265a0778a880afb"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  type = string
+}
+
+variable "tags" {
+  type = map
+  default = {
+    Name = "Hello Terraform"
+    Project = "Roboshop"
+    Environment = "DEV"
+    Component = "Web"
+    Terraform = "true"
+  }
+}
+
+variable "sg-name" {
+  type = string
+  default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "allowing all ports"
+}
+
+variable "inbound-from-port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
